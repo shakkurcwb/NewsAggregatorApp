68db1978fcfd6c65fdbfce109cbd221b68b5877b
Marlon Ferreira <ma.ferreira93@gmail.com>
2023-09-01 22:45:05 -0400

    reorder home

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -7,6 +7,15 @@
             <div class="col-md-4 mb-4">
                 <div class="card">
                     <div class="card-body">
+                        <h5 class="card-title">Live News Feed</h5>
+                        <p class="card-text">Provides instant, topic-specific updates.</p>
+                        <a href="{{ url('/live') }}" class="btn btn-primary">Go to Live News</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
                         <h5 class="card-title">Manage Rss Feeds</h5>
                         <p class="card-text">Effortlessly create and manage RSS feeds.</p>
                         <a href="{{ url('/rss-feeds') }}" class="btn btn-primary">Go to Rss Feeds</a>
@@ -17,17 +26,8 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Search News Articles</h5>
-                        <p class="card-text">Discover and access relevant and up-to-date information.</p>
+                        <p class="card-text">Explore real-time and historical news.</p>
                         <a href="{{ url('/articles') }}" class="btn btn-primary">Go to News Articles</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Live News Feed</h5>
-                        <p class="card-text">Provides instant, topic-specific updates.</p>
-                        <a href="{{ url('/live') }}" class="btn btn-primary">Go to Live News</a>
                     </div>
                 </div>
             </div>
